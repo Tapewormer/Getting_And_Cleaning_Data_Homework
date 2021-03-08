7e084096150fd97e6db6ccd736215c41edbbda24
Tapewormer <pierre.jonniaux@gmail.com>
2021-03-08 17:29:42 +0100

    The codebook required by the teacher.

---
+++ CodeBook.md
@@ -0,0 +1,46 @@
+### CODEBOOK FOR THE "GETTING AND CLEANING DATA" HOMEWORK ###
+
+This file describes the detail of the data used and the transformation applied to the data. See README.txt for more general information.
+
+The data set is taken from :
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip  
+
+###
+FILES used by the script are :
+# Metadata
+- 'features.txt'       : List of all features meaning the type of observations. There are 561 types of observations.
+- 'activity_labels.txt': Links the class labels with their activity name. There are 6 physical activities ("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING"). The script uses directly a user-created list instead of the actual file.
+
+# Training group
+- 'train/X_train.txt'  : Training set with the observations for the training group subjects.The training group consist of 21 subject resulting in 7352 observations.
+- 'train/y_train.txt'  : Training labels with the activity for the the training group subjects. The training group consist of 21 subjects resulting in 7352 observations. 
+- 'subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+
+# Testing group
+- 'test/X_test.txt'    : Test set with the observations for the test group subjects. The test group consist of 9 subjects resulting in 2947 observations.
+- 'test/y_test.txt'    : Test labels with the activity for the the test group subjects. The test group consist of 9 subjects resulting in 2947 observations.
+- 'subject_test.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30.
+
+(For more details on the files & experimental observations see README.txt file located within the original data folder)
+###
+
+###
+# SCRIPT - DATA TRANSFORMATION
+The script is named "run_analysis.R"
+
+1 - After download, each of the 3 files from the training group (X_train, y_train, subject_train) are merged horizontally with its testing group counterpart and stored into 3 tibble objects : subjects_all, activity_all, variable_all
+2 - subjects_all's only column is renamed "subject_id"
+3 - activity_all's only column is renamed "activity"
+4 - activity_all values are transformed from their numeric label to descriptive word (e.g. "1" becomes "WALKING") 
+5 - variable_all's 561 columns are renamed according to the type of observations they contains as provided by the file "features.txt"
+6 - variable_all is then reduced to an object containing only the observations related to means and standard deviations resulting into a 66 columns dataframe.
+7 - Each of the 3 curated objects subjects_all, activity_all, variable_all are merged vertically into a dataframe named
+merged_all of dimension (7352+2947) = 10299 rows and (1+1+66) = 68 columns. The first two columns being the subject_id and the type of activity.
+8 - merged_all is grouped by two factors "subject_id" and "activity". The average of each variable for each activity and each subject is computed. 
+9 - The result of the computation is stored in a comma separated file named "UCI_HAR_tidy_dataset.csv".  
+###
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,85 @@
+# Getting and Cleaning Data Course Project #
+
+# Goals :
+# 1 - Merges the training and the test sets to create one data set.
+# 2 - Extracts only the measurements on the mean and standard deviation for 
+#     each measurement. 
+# 3 - Uses descriptive activity names to name the activities in the data set
+# 4 - Appropriately labels the data set with descriptive variable names. 
+# 5 - From the data set in step 4, creates a second, independent tidy data set 
+#     with the average of each variable for each activity and each subject.
+
+library(tibble)
+library(dplyr)
+
+# Download and unzip the files
+file_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if (!file.exists("./UCI_HAR_dataset.zip"))
+  download.file(file_url, "./UCI_HAR_dataset.zip", method = "curl")
+  unzip("UCI_HAR_dataset.zip")
+
+# Store needed files into data objects :
+# Store - Test Batch
+subjects_test <- as_tibble(read.table("UCI HAR Dataset/test/subject_test.txt"))
+activity_test <- as_tibble(read.table("UCI HAR Dataset/test/y_test.txt"))
+variable_test <- as_tibble(read.table("UCI HAR Dataset/test/X_test.txt"))
+
+# Store - Training Batch
+subjects_train <- as_tibble(read.table("UCI HAR Dataset/train/subject_train.txt"))
+activity_train <- as_tibble(read.table("UCI HAR Dataset/train/y_train.txt"))
+variable_train <- as_tibble(read.table("UCI HAR Dataset/train/X_train.txt"))
+
+# Store - Metadata 
+# "features.txt" has what will be used for experiments variables names (516 col)
+features_tbl <- as_tibble(read.table("UCI HAR Dataset/features.txt"))
+namesforcol <- as.character(select(features_tbl, 2)[[1]])
+
+# Merge training and test set together
+subjects_all <- bind_rows(subjects_train, subjects_test)
+activity_all <- bind_rows(activity_train, activity_test)
+variable_all <- bind_rows(variable_train, variable_test)
+
+# Prepare the data
+# Prepare - the subject column
+subjects_all <- subjects_all %>% rename(subject_id = V1)
+
+# Prepare - the activity column
+# A dictionary-like list (used for step3 : activity numbers into words)
+activity_dic <- c("1" = "WALKING",
+                  "2" = "WALKING_UPSTAIRS",
+                  "3" = "WALKING_DOWNSTAIRS",
+                  "4" = "SITTING",
+                  "5" = "STANDING",
+                  "6" = "LAYING")
+# Rename the column and convert numers to descriptive word
+activity_all <- activity_all %>%
+                rename(activity = V1) %>% 
+                mutate(activity = activity_dic[activity])
+
+
+# Prepare - the variable columns
+# Rename columns (step 4 requirement) 
+colnames(variable_all) <- namesforcol
+# select only those with mean and stdev
+variable_all <- variable_all %>% select(matches("\\-mean\\(\\)|\\-std\\(\\)"))
+
+
+# Merge
+# Merge subject, label and experiment results together
+merged_all <- bind_cols(subjects_all, activity_all, variable_all)
+
+# Analyse
+# average of the 66 experiments variables (mean and stdv only) split across the
+# subject and the activity. 30 subjects x 6 activities resulting into 180 obs.
+result <- merged_all %>%
+          group_by(subject_id, activity, .add = TRUE) %>%
+          summarise(across(1:66, mean))
+
+# Save the results in a file
+write.csv(result, file="UCI_HAR_tidy_dataset.csv", row.names=FALSE)
+          
+
+
+
+
+
